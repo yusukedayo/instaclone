9fc1848e08a2c2ac5489ddc07a08a86e23e0d659
yusuke <89657123+yusukedayo@users.noreply.github.com>
2021-12-22 21:16:32 +0900

    sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new'  
+  post 'login', to: 'user_sessions#create'  
+  delete 'logout', to: 'user_sessions#destroy'
+  resources :users, only: %i(new create)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :tweet
+  root to: 'users#new'
 end
