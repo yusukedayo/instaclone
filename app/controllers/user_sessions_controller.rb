233fa066814c75bead789e79af3bbc12799ff0a0
yusukedayo <89657123+yusukedayo@users.noreply.github.com>
2021-12-30 21:05:38 +0900

    viewを追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,21 +1,19 @@
 class UserSessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(new_user_path, notice: 'ログインに成功しました')
-      
+      redirect_back_or_to(new_user_path, notice: "ログインに成功しました")
     else
-      flash.now[:alert] = 'ログインに失敗しました'
+      flash.now[:alert] = "ログインに失敗しました"
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to(login_path, notice: 'ログアウトしました')
+    redirect_to(root_path, notice: "ログアウトしました")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'users#new'
+
+  resources :users, only: %i(new create)
   get 'login', to: 'user_sessions#new'  
   post 'login', to: 'user_sessions#create'  
   delete 'logout', to: 'user_sessions#destroy'
-  resources :users, only: %i(new create)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#new'
 end
