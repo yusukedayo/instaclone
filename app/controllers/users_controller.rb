7e7ec9c4f4235461630c785ebae0d521c1fa756a
yusukedayo <89657123+yusukedayo@users.noreply.github.com>
2021-12-30 21:05:38 +0900

    gitignoreとviewの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
-      flash[:notice] = 'ユーザーの作成に成功しました'
+      auto_login(@user)
+      redirect_to login_path, notice: 'ユーザーの作成に成功しました'
     else
       flash.now[:alert] = 'ユーザーの作成に失敗しました'
       render :new
